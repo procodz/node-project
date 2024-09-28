4efb38d1cefc38cbc23aaf197fa857206d881b00
Raghvendra <raghu0666kumar@gmail.com>
2024-09-28 10:13:56 +0530

    making forget password api (self)

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleweres/auth");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //Profile API
 profileRouter.post("/profile", userAuth, async (req,res) => {
@@ -32,13 +34,10 @@ profileRouter.patch("/user/:userId", async (req,res) => {
     const data = req.body;
     
     try{
-      const Allowed_Update = ["firstName", "age", "gender", "skills"];
-      const isUpdateAllowed = Object.keys(data).every((k) => Allowed_Update.includes(k));
+      const Allowed_Update = ["age", "gender", "skills"];
+      const isUpdateAllowed = Object.keys(data).every((k) => Allowed_Update.includes(k));// loop through all the data(req.body) and checks if allowed field includes data(req.body)
       if(!isUpdateAllowed){
           throw new Error("Invalid Update...");
-      }
-      if(data.skills.length > 8){
-          throw new Error("skill can not be more than 8...")
       }
       const updatedUser = await User.findByIdAndUpdate({_id: userId}, data, {
           runValidators: true,
@@ -48,6 +47,29 @@ profileRouter.patch("/user/:userId", async (req,res) => {
     }
     catch (err){
       res.status(404).send("ERROR " + err.message);
+    }
+  });
+
+  profileRouter.post("/logout", (req,res) => {
+    res.cookie("token", null, {           //setting token value to null this will delete the cookie and u will be logged out.
+        expires: new Date(Date.now())
+    }).send('you have been loged out successfully!');
+  })
+
+  profileRouter.patch("/forgetPassword", userAuth, async (req,res) => {
+    try {
+    const user = req.user; //getting user from req
+    const currentPass = req.body.currentPass; //input by user
+    const newPassword = req.body.newPassword;//input by user
+    const checkingPass = await bcrypt.compare(currentPass, user.password);//checking if currentpass is the user pass
+    if(!checkingPass){
+        throw new Error("Invalid credentials...")
+    }
+    user.password = await bcrypt.hash(newPassword, 10);//changing current pass to newpass
+    await user.save();//saving the data
+    res.send("password is succesfully updated.");
+}catch(err) {
+        res.status(400).send("ERROR: " + err.message);
     }
   });
 
